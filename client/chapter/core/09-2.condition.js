64c3903ade7fc87679596adff296fa1b448d6e7e
otwaylee <ahyeong858@gmail.com>
2024-06-02 20:23:48 +0900

    5/31 JS Info - condition 까지

---
+++ client/chapter/core/09-2.condition.js
@@ -0,0 +1,32 @@
+/* ------------------- */
+/* Logical Operators   */
+/* ------------------- */
+
+let a = 10;
+let b = '';
+let value = Boolean(b);
+
+// 논리곱(그리고) 연산자
+let AandB = a && b;
+console.log('AandB :',AandB)
+
+//논리곱 할당 연산자 Logical AND Assignment
+// a &&= b
+// 논리합(또는) 연산자
+let AorB= a || b;
+console.log('AorB :',AorB)
+
+// 논리 합 할당 연산자 
+// a ||= b
+// 부정 연산자
+let reverseValue = !value;
+console.log('reverseValue :',reverseValue)
+
+
+// 조건 처리
+
+// 첫번째 Falsy를 찾는 연산 (&&)
+// let whichFalsy= true && ' ' && [] && {thisIsFalsy:false};
+// console.log('whichFalsy :',whichFalsy)
+// 첫번째 Truthy를 찾는 연산 (||)
+// let whichTruthy= false || '' || [2,3].length || {thisIsFalsy:false};  
