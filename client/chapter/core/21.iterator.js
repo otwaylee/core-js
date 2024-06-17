c1031bf26dee052120c1241b71b479ee60ab1050
otwaylee <ahyeong858@gmail.com>
2024-06-17 13:41:44 +0900

    6/17 JS Info - try catch

--- client/chapter/core/21.iterator.js
+++ client/chapter/core/21.iterator.js
@@ -25,6 +25,8 @@ console.log(iter.next().value);
 console.log(iter.next().value);
 
 
+
+
 const range = {
   from: 1,
   to: 5,
@@ -46,8 +48,54 @@ const range = {
 }
 
 
+
+
+
+
+function* gen(){
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+const gene = gen()
+
+const customIter = {
+  *[Symbol.iterator](){
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+}
+
+for(const a of customIter){
+  console.log(a)
+}
+
+
 // 유사배열 vs. 이터러블
 // - 유사배열 : 인덱스 키와 length 속성을 가진 객체
 // - 이터러블 : Symbol.Iterator 메서드를 가지는 객체
 
 // 유사배열, 이터러블 배열화
+
+
+function* idGenerator(){
+  let id = 1;
+  while(true){
+    yield ++id
+  }
+}
+
+
+const id = idGenerator();
+
+
+
+// 1. 일관된 반복 동작 제공 ( for ..of )
+// 2. 커스텀 반복 제어 가능 (객체를 반복 가능한 상태로 )
+// 3. 지연 계산 (필요할 때마다 반복을 돌림)
+// 4. 무한 시퀀스 생성(무한대의 값 생성)
+// 5. 비동기 반복 작업 
+// 6. 다양한 데이터 소스와의 통합(Map, Set)
+
