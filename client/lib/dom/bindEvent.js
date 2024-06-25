b7c741817dc83f064c41689bf3f38dbfd0032339
otwaylee <ahyeong858@gmail.com>
2024-06-25 10:18:06 +0900

    learn jujeob generator

--- client/lib/dom/bindEvent.js
+++ client/lib/dom/bindEvent.js
@@ -14,4 +14,3 @@ export function bindEvent(node,type,handler){
 }
 
 
-
--- client/lib/dom/index.js
+++ client/lib/dom/index.js
@@ -1,9 +1,7 @@
 
 
 
-// attr 함수를 받아옴
 // 다시 내보내기 re-export
-
 
 
 export * from './css.js';
@@ -11,8 +9,9 @@ export * from './attr.js';
 export * from './insert.js';
 export * from './getNode.js';
 export * from './bindEvent.js';
+export * from './showAlert.js';
 export * from './clearContents.js';
-
+export {default as clearContents} from './clearContents.js';
 
 
 
